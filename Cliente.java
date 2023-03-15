4f1db21741f150b4a823d5ee6b686c3d119b5362
Daniel Almeida Pereira <daniel.pereira@germinare.org.br>
2023-03-15 19:37:01 -0300

    resolvendo --CRIANDO CONFLITO kkkk--

--- Cliente.java
+++ Cliente.java
@@ -1,9 +1,6 @@
 public class Cliente {
-<<<<<<< HEAD
     private String daniel;
-=======
     private String Willamy;
->>>>>>> origin/classe-cliente
     private String cpf;
     private int agencia;
     private double saldo;
