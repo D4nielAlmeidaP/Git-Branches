c978489fdc4568e33c236ba29bd17266c7a464bf
Daniel Almeida Pereira <daniel.pereira@germinare.org.br>
2023-03-15 19:47:41 -0300

    deu certo

--- Cliente.java
+++ Cliente.java
@@ -1,13 +1,10 @@
 public class Cliente {
-<<<<<<< HEAD
 
     private String tipoConta;
 
     private String nome;
-=======
     private String daniel;
     private String Willamy;
->>>>>>> classe-cliente
     private String cpf;
     private int agencia;
     private double saldo;
